0ae0ded62cbf2dab575fd762e72cda0603760c9b
Fazley <22FTT1494@student.pb.edu.bn>
2024-03-12 00:04:51 +0800

    Add routes for offers and tabs pages

--- places/src/app/app.routes.ts
+++ places/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
 
 export const routes: Routes = [
- 
   {
     path: '',
     redirectTo: 'auth',
@@ -9,14 +9,37 @@ export const routes: Routes = [
   },
   {
     path: 'auth',
-    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
+
   {
-    path: 'places',
-    loadComponent: () => import('./places/places.page').then( m => m.PlacesPage)
+    path: 'offers',
+    loadComponent: () =>
+      import('./offers/offers.page').then((m) => m.OffersPage),
   },
   {
     path: 'tabs',
-    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
+    component: TabsPage,
+    children: [
+      {
+        path: 'places',
+        loadComponent: () =>
+          import('./places/places.page').then((m) => m.PlacesPage),
+      },
+      {
+        path: 'offers',
+        loadComponent: () =>
+          import('./offers/offers.page').then((m) => m.OffersPage),
+      },
+    ],
+  },
+  {
+    path: 'places',
+    redirectTo: '/tabs/places',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./tabs/tabs.page').then((m) => m.TabsPage),
   },
 ];
